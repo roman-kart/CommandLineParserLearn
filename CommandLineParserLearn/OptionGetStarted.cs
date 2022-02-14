54529e39e2f7d2c2fb86d9be774de76117f2cdb1
roman-kart <81640683+roman-kart@users.noreply.github.com>
2022-02-14 21:21:45 +0300

    Прочитал GetStarted

---
+++ CommandLineParserLearn/OptionGetStarted.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using CommandLine;
+namespace CommandLineParserLearn
+{
+    public class OptionGetStarted
+    {
+        [Option("cat", Default = "Hello, Cat!")]
+        public string Cat { get; set; }
+        [Option("tac", Default = "qqq")]
+        public string Tac { get; set; }
+    }
+}
--- CommandLineParserLearn/Program.cs
+++ CommandLineParserLearn/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using CommandLine;
+using CommandLineParserLearn;
+
+// default options
+var result = Parser.Default.ParseArguments<OptionGetStarted>(args);
+result.WithParsed(p => System.Console.WriteLine(p.Cat));
+result.WithParsed(p => System.Console.WriteLine(p.Tac));
