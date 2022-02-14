79382e2aed3c5910100da9e89830a4144d5e39a7
roman-kart <81640683+roman-kart@users.noreply.github.com>
2022-02-14 21:34:18 +0300

    Прочитал раздел Command Line Grammar

---
+++ CommandLineParserLearn/OptionsDocs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace CommandLineParserLearn
+{
+    public class OptionsDocs
+    {
+        [Option('r', "reverse", Default = false)]
+        public bool Reverse { get; set; }
+        [Option('t', "text", Required = false)]
+        public string Text { get; set; }
+        [Option('s', "states", Required = false)]
+        public IEnumerable<string> Statements { get; set; }
+    }
+}
--- CommandLineParserLearn/Program.cs
+++ CommandLineParserLearn/Program.cs
@@ -1,7 +1,7 @@
-﻿using CommandLine;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using CommandLine;
 using CommandLineParserLearn;
 
-// default options
-var result = Parser.Default.ParseArguments<OptionGetStarted>(args);
-result.WithParsed(p => System.Console.WriteLine(p.Cat));
-result.WithParsed(p => System.Console.WriteLine(p.Tac));
+Console.WriteLine("TODO: add ");
